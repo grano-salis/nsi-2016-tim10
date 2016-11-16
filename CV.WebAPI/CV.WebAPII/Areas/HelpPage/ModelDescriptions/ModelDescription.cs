edbf4f1d19bb393312d93cfef65731eb25586f10
HumaMilisic <huma.milisic@gmail.com>
2016-11-16 06:26:03 +0100

    APII

---
+++ CV.WebAPI/CV.WebAPII/Areas/HelpPage/ModelDescriptions/ModelDescription.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CV.WebAPII.Areas.HelpPage.ModelDescriptions
+{
+    /// <summary>
+    /// Describes a type model.
+    /// </summary>
+    public abstract class ModelDescription
+    {
+        public string Documentation { get; set; }
+
+        public Type ModelType { get; set; }
+
+        public string Name { get; set; }
+    }
+}
