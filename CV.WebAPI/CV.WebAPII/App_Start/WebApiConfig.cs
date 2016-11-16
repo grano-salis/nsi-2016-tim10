a9ed68444847f820b76275d65477115285b6680f
HumaMilisic <huma.milisic@gmail.com>
2016-11-16 20:16:37 +0100

    Together

--- CV.WebAPI/CV.WebAPII/App_Start/WebApiConfig.cs
+++ CV.WebAPI/CV.WebAPII/App_Start/WebApiConfig.cs
@@ -12,8 +12,9 @@ namespace CV.WebAPII
             // Web API configuration and services
 
             // Web API routes
+            config.EnableCors();
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
