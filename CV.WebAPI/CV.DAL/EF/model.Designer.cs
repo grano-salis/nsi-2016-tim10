7fee65e32f8747d5211e89e75911d3db2a3b99c6
Strojki <estrojil1@etf.unsa.ba>
2016-12-15 17:56:46 +0100

    Did some style for directives from CV folder. Made some changes for Login module.

--- CV.WebAPI/CV.DAL/EF/model.Designer.cs
+++ CV.WebAPI/CV.DAL/EF/model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\WorkIt\Desktop\nsi-2016-tim8\CV.WebAPI\CV.DAL\EF\model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Edin\WebstormProjects\nsi-2016-tim8\CV.WebAPI\CV.DAL\EF\model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
