bfe8c79698d464855a649b5fbfc96a4d2e3baca8
HumaMilisic <huma.milisic@gmail.com>
2016-12-23 20:13:45 +0100

    Modal for workExperience created

--- CV.WebAPP/src/app/components/cv/skillsPassport/learnerInfo/workExperience/workExperience.directive.js
+++ CV.WebAPP/src/app/components/cv/skillsPassport/learnerInfo/workExperience/workExperience.directive.js
@@ -1,3 +1,95 @@
-/**
- * Created by WorkIt on 21/12/2016.
- */
+(function() {
+  'use strict';
+
+  angular
+    .module('ea.cv')
+    .directive('workExperience', directive);
+
+  /** @ngInject */
+  function directive() {
+    var directive = {
+      // restrict: 'E',
+      templateUrl: 'app/components/cv/skillsPassport/learnerInfo/workExperience/workExperience.tmpl.html',
+      controller: ctrl,
+      scope: {
+        'model':'=model'
+      }
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function ctrl($scope,$uibModal,$log) {
+      $scope.odabrani = $scope.model;
+
+      var openModal = function (model,func) {
+        var modalInstance = $uibModal.open({
+          ariaLabelledBy: 'modal-title',
+          ariaDescribedBy: 'modal-body',
+          templateUrl: 'app/components/cv/skillsPassport/learnerInfo/workExperience/workExperienceModal.tmpl.html',
+          controller: function ($scope,$uibModalInstance,model) {
+            $scope.model = model;
+
+            $scope.ok = function () {
+              //$uibModalInstance.close($ctrl.selected.item);
+              debugger;
+              //alert('yo');
+              // $scope.send();
+              $uibModalInstance.close($scope.model);
+            };
+
+            $scope.cancel = function () {
+              $uibModalInstance.dismiss('cancel');
+            };
+          },
+          controllerAs: '$ctrl',
+          resolve: {
+            model: function () {
+              return model;
+            }
+          }
+        });
+
+        modalInstance.result.then(function (newModel) {
+          //$ctrl.selected = selectedItem;
+          debugger;
+          //model = newModel;
+          func && func(newModel);
+        }, function () {
+          $log.info('Modal dismissed at: ' + new Date());
+        });
+      }
+
+      $scope.addNewModal = function () {
+        var add=null;
+        openModal(add,function (add) {
+          debugger
+          if($scope.odabrani==null || typeof ($scope.odabrani)=='undefined')
+            $scope.odabrani = [];
+
+          $scope.odabrani.push(add);
+        });
+
+      };
+
+      $scope.editModal=function($index){
+        openModal($scope.odabrani[$index],function (edit) {
+          $scope.odabrani[$index]=edit;
+        });
+      }
+
+      $scope.delete = function($index){
+        $scope.odabrani.splice($index);
+      };
+
+      if($scope.odabrani)
+        $scope.model = $scope.odabrani;
+
+      $scope.$watch('odabrani',function(){
+        if($scope.odabrani)
+          $scope.model = $scope.odabrani;
+      });
+    }
+  }
+
+})();
